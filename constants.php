cc794b300c25848eeb5b3b238da5fc01a6081bb4
Lester Jan Gamier <gamier.lester@gmail.com>
2019-01-17 23:02:04 +0800

    Includes test deployment for heroku

--- constants.php
+++ constants.php
@@ -1,9 +1,13 @@
 <?php
-	define("DBUSER", "root");
-	define("DBPASS", "");
-	define("DBHOST", "localhost");
-	define("DBNAME", "assessment-db");
-
+	// define("DBUSER", "root");
+	// define("DBPASS", "");
+	// define("DBHOST", "localhost");
+	// define("DBNAME", "assessment-db");
+	define("DB_USER", "juancho");
+	define("DB_PWORD", "foreverinlov3");
+	define("DB_NAME", "todoapponetwo");
+	define("SERVER_NAME", "db4free.net");
+	
 	if(isset($_SERVER['HTTP_ORIGIN'])){
 		header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
 		header('Access-Control-Allow-Credentials: true');
